bdf995f4781b1d9d6f8dc677b6daffcc7d172bbe
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-03-04 17:17:45 -0300

    add initial LocalSavePurchases tdd

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,5 +1,34 @@
+class LocalSavePurchases {
+  constructor(private readonly cacheStore: CacheStore) {}
+
+  async save(): Promise<void> {
+    this.cacheStore.delete();
+  }
+}
+
+interface CacheStore {
+  delete: () => void;
+}
+
+class CacheStoreSpy implements CacheStore {
+  deleteCallsCount = 0;
+
+  delete(): void {
+    this.deleteCallsCount++
+  }
+}
+
 describe('LocalSavePurchases', () => {
-  test('', () => {
-    expect(1).toBe(1);
-  })
+  test('Should not delete cache on init', () => {
+    const cacheStore = new CacheStoreSpy();
+    new LocalSavePurchases(cacheStore);
+    expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test('Should delete old cache on save', async () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore);
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
+  });
 })
