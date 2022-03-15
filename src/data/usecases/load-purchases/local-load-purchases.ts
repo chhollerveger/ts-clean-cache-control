996888a94140b53a176949e0ba0c1d9996018b04
Carlos Hollerveger <carlos.hollerveger@ntconsult.com.br>
2022-03-15 18:23:18 -0300

    ensure delete old cache on validate if cache is expired

--- src/data/usecases/load-purchases/local-load-purchases.ts
+++ src/data/usecases/load-purchases/local-load-purchases.ts
@@ -33,7 +33,10 @@ export default class LocalLoadPurchases implements SavePurchases, LoadPurchases 
 
   validate(): void {
     try {
-      this.cacheStore.fetch(this.key);
+      const cache = this.cacheStore.fetch(this.key);
+      if (!CachePolicy.validate(cache.timestamp, this.currentDate)) {
+        throw new Error();
+      }
     } catch (error) {
       this.cacheStore.delete(this.key);
     }
